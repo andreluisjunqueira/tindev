487c55041190c94ea33aca4131a4aa6452e63677
Andre Junqueira <andre@MacBook-Pro-de-Andre.local>
2019-11-07 12:03:31 -0300

    first commit

---
+++ src/pages/Login.js
@@ -0,0 +1,82 @@
+import React, { useState, useEffect } from 'react';
+import { KeyboardAvoidingView, Text, Image, TextInput, StyleSheet, TouchableOpacity, Platform } from 'react-native';
+import AsyncStorage from '@react-native-community/async-storage';
+
+import Logo from '../assets/logo.png';
+import api from '../services/api';
+
+export default function Login({navigation}) {
+  const [user, setUser] = useState('');
+
+  useEffect(()=>{
+    (async function(){
+      const user = await AsyncStorage.getItem('user');
+      if(user){
+        navigation.navigate('Main', { user });
+      }
+    })();
+  },[]);
+
+  async function handleLogin(){
+    const response = await api.post('/devs', {
+      username: user
+    });
+
+    const { _id } = response.data;
+
+    await AsyncStorage.setItem('user', _id);
+
+    navigation.navigate('Main', { user: _id });
+  }
+
+  return (
+    <KeyboardAvoidingView 
+      behavior="padding"
+      enabled={Platform.OS == 'ios'}
+      style={styles.container}>
+      <Image source={Logo}/>
+      <TextInput
+        placeholder="Digite seu usuário no Github"
+        style={styles.input}
+        autoCapitalize="none"
+        autoCorrect={false}
+        onChangeText={text => setUser(text)}
+      />
+      <TouchableOpacity style={styles.button} onPress={handleLogin}>
+        <Text style={styles.buttonText}>Entrar</Text>
+      </TouchableOpacity>
+    </KeyboardAvoidingView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#f5f5f5',
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 30
+  },
+  input: {
+    height: 40,
+    alignSelf: 'stretch',
+    backgroundColor: '#fff',
+    borderWidth: 1,
+    borderColor: '#ddd',
+    marginTop: 20,
+    paddingHorizontal: 15
+  },
+  button: {
+    height: 46,
+    alignSelf: 'stretch',
+    backgroundColor: '#df4723',
+    marginTop: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  buttonText: {
+    color: '#fff',
+    fontWeight: 'bold',
+    fontSize: 16
+  }
+})
